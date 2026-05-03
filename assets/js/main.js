0c278a255e3f0a17293fe80c0e6ada69693b8eed
Zalam2004 <dal638779@utdallas.edu>
2026-05-03 19:02:42 +0000

    Updated code

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,27 @@
 // main.js
 
-console.log("Hello, World!");
+let correctAnswer = "";
+
+function loadQuestion() {
+    fetch("https://opentdb.com/api.php?amount=50");
+    .then(res => res.json())
+    .then(data => {
+        const q = data.results[0];
+
+        const temp = document.createElement("textarea");
+        temp.innerHTML = q.question;
+
+        document.getElementById("question").innerText = temp.value;
+        correctAnswer = q.correct_answer;
+        document.getElementById("feedback").innerText = "";
+    });
+}
+function checkAnswer(answer) {
+    if (answer === correctAnswer) {
+        document.getElementById("feedback").innerText = "Correct!";
+} else {
+    document.getElementById("feedback").innerText = "Wrong!";
+}
+setTimeout(loadQuestion, 50);
+}
+loadQuestion();
