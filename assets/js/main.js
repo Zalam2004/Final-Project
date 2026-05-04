c398908f73f346a8b4a9473ffe0a37d94bd0f966
Zalam2004 <dal638779@utdallas.edu>
2026-05-04 23:27:52 +0000

    updated code

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,33 +1,54 @@
 // main.js
 
-let currentQuestion = null;
+let questionData = null;
 let correctAnswer = "";
 
 function loadQuestion() {
     fetch("https://opentdb.com/api.php?amount=1&type=boolean")
-    .then(res => res.json())
+    .then(response => response.json())
     .then(data => {
-        const currentQuestion = data.results[0];
+        const questionData = data.results[0];
 
-        const temp = document.createElement("textarea");
-        temp.innerHTML = currentQuestion;
-
-        document.getElementById("question").textContent = temp.value;
-        correctAnswer = currentQuestion.correct_answer;
-        document.getElementById("result").textContent = "";
+        document.getElementById("question").innerHTML = questionData.question;
+        correctAnswer = questionData.correct_answer;
+        document.getElementById("result").innerHTML = "";
+    })
+    .catch(error => {
+        document.getElementById("question").innerHTML = "Error: Can't load question.";
     });
 }
 function checkAnswer(userChoice) {
     if (!correctAnswer) return;
     
     if (userChoice === correctAnswer) {
-        document.getElementById("result").textContent = "Right!";
+        document.getElementById("result").innerHTML = "Right!";
     } else {
-        document.getElementById("result").textContent = "Wrong! Correct Answer: " + correctAnswer;
+        document.getElementById("result").innerHTML = "Wrong! Correct Answer: " + correctAnswer;
     }
     setTimeout(currentQuestion, 1500);
 }
 loadQuestion();
+
+let dares = null;
+
+function loadDare () {
+    fetch("https://fungenerators.com/dares.json")
+    .then(response => response.json())
+    .then(data => {
+        const dares = data.dares;
+
+        const randomIndex = Math.floor(Math.floor() * dares.length);
+        const randomDare = dares[randomIndex];
+
+        document.getElementById("dare").textContent = randomDare;
+    })
+    .catch(error => {
+        document.getElementById("dare").textContent = "Error. Can't load dare.";
+        console.log(error);
+    });
+    setTimeout(dares, 1500);
+}
+loadDare();
 
         
 
