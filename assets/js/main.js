41a3d480f12df25bfc8d5fa84256377daf84e80d
Zalam2004 <dal638779@utdallas.edu>
2026-05-04 01:22:00 +0000

    Updated code

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,27 +1,29 @@
 // main.js
 
-let correctAnswer = "";
+let currentQuestion = null;
 
 function loadQuestion() {
-    fetch("https://opentdb.com/api.php?amount=50");
+    fetch("https://opentdb.com/api.php?amount=1&type=boolean")
     .then(res => res.json())
     .then(data => {
-        const q = data.results[0];
+        currentQuestion = data.results[0];
 
         const temp = document.createElement("textarea");
-        temp.innerHTML = q.question;
+        temp.innerHTML = currentQuestion.question;
 
         document.getElementById("question").innerText = temp.value;
-        correctAnswer = q.correct_answer;
-        document.getElementById("feedback").innerText = "";
+        document.getElementById("result").innerText = "";
     });
 }
-function checkAnswer(answer) {
-    if (answer === correctAnswer) {
-        document.getElementById("feedback").innerText = "Correct!";
-} else {
-    document.getElementById("feedback").innerText = "Wrong!";
-}
-setTimeout(loadQuestion, 50);
+
+function answer(userChoice) {
+    if (userChoice === currentQuestion.correct_answer) {
+        document.getElementById("result").innerText = "Correct!";
+    } else {
+        document.getElementById("result").innerText = "Wrong! Answer: " + currentQuestion.correct_answer;
+    }
+    setTimeout(loadQuestion, 1500);
 }
 loadQuestion();
+
+
